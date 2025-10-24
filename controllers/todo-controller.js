e67775c2f8a4b2e163faf366b838a3f05e9e88aa
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-10-24 23:22:27 +0530

    updated getTodoList, initialized insertTodoList

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -4,7 +4,33 @@ export const getTodoListController = async(req,res) => {
     try {
         const result = await pool.query('select * from main.todo_collection')
         console.log(result.rows)
-        return res.status(200).json(result.rows)
+        let todoList = result.rows
+        let formattedTodoList = []
+        for(let i = 0; i < todoList.length; i++){
+            const {todo_id, title, description, created_on, updated_on, is_done, is_deleted} = todoList[i]
+            formattedTodoList.push({
+                id : todo_id,
+                title : title,
+                description : description,
+                done : is_done,
+                delete : is_deleted,
+                updated : !(updated_on === null),
+                createdOn : created_on,
+                updatedOn : updated_on
+            })
+        }
+        return res.status(200).json(formattedTodoList)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message:"Something went wrong!"})
+    }
+}
+
+export const insertTodoListController = async(req,res) => {
+    try {
+        const {title, description} = req.body 
+        await pool.query(`insert into main.todo_collection(title, description) values ( $1, $2 )`,[title,description])      
+        return res.status(200).json({message: "Success!"})  
     } catch (error) {
         console.log(error)
         return res.status(500).json({message:"Something went wrong!"})
--- routes/todo-routes.js
+++ routes/todo-routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getTodoListController } from '../controllers/todo-controller.js';
+import { getTodoListController, insertTodoListController } from '../controllers/todo-controller.js';
 
 const router = express.Router();
 
 router.get("/task", getTodoListController)
+
+router.post("/task",insertTodoListController)
 
 export default router;
